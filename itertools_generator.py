9cf17bfa1c3fc05c46aab6582fdfbb56b2b13a70
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-21 20:29:48 +0000

    Non-working attempt to zip

--- itertools_generator.py
+++ itertools_generator.py
@@ -1,21 +1,17 @@
 import itertools
 
-
-def all_positive_integers():
-    number_to_return = 1
-    while True:
-        yield number_to_return
-        number_to_return += 1
-
-
 if __name__ == '__main__':
-    square_numbers = (x * x for x in all_positive_integers())
-    # count = 10
-    # for x in square_numbers:
-    #     print(x)
-    #     count -= 1
-    #     if count < 0:
-    #         break
-    square_numbers_limited = (x * x for x in itertools.islice(all_positive_integers(), 10))
-    for x in square_numbers_limited:
+    all_positive_integers = (x+1 for x in itertools.count())
+    all_squares = (x * x for x in all_positive_integers)
+    all_evens = (2 * x for x in all_positive_integers)
+    zipped = (x for sublist in zip(all_squares, all_evens) for x in sublist)
+    # Doesn't work: exercise for the reader
+    count = 10
+    for x in zipped:
         print(x)
+        count -= 1
+        if count < 0:
+            break
+    # square_numbers_limited = (x * x for x in itertools.islice(all_positive_integers(), 10))
+    # for x in square_numbers_limited:
+    #     print(x)
