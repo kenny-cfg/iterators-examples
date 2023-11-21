f20be8c193cbf3a2b44bf5fa11a1b6c3c45626e9
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-21 22:18:53 +0000

    Working example with zip

--- itertools_generator.py
+++ itertools_generator.py
@@ -1,11 +1,9 @@
 import itertools
 
 if __name__ == '__main__':
-    all_positive_integers = (x+1 for x in itertools.count())
-    all_squares = (x * x for x in all_positive_integers)
-    all_evens = (2 * x for x in all_positive_integers)
+    all_squares = (x * x for x in itertools.islice(itertools.count(), 1, None))
+    all_evens = (2 * x for x in itertools.islice(itertools.count(), 1, None))
     zipped = (x for sublist in zip(all_squares, all_evens) for x in sublist)
-    # Doesn't work: exercise for the reader
     count = 10
     for x in zipped:
         print(x)
