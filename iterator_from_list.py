f1844915905a31957d178e05bff8140a56850501
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-21 18:41:31 +0000

    Iterator from a list

---
+++ iterator_from_list.py
@@ -0,0 +1,15 @@
+nums = [2, 4, 1, 9, 6]
+
+nums_iter = iter(nums)
+
+if __name__ == '__main__':
+    print(nums_iter)
+    for x in nums_iter: # Knows to stop
+        print(x)
+
+    print(next(nums_iter))
+    print(next(nums_iter))
+    print(next(nums_iter))
+    print(next(nums_iter))
+    print(next(nums_iter))
+    print(next(nums_iter)) # Throws an exception
