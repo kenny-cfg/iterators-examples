bc5c93ee0bfe6e1bcbbd845956c6ef2d984e25ed
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-21 18:42:37 +0000

    __next__() function

--- iterator_from_list.py
+++ iterator_from_list.py
@@ -7,9 +7,18 @@ if __name__ == '__main__':
     for x in nums_iter: # Knows to stop
         print(x)
 
-    print(next(nums_iter))
-    print(next(nums_iter))
-    print(next(nums_iter))
-    print(next(nums_iter))
-    print(next(nums_iter))
-    print(next(nums_iter)) # Throws an exception
+    # print(next(nums_iter))
+    # print(next(nums_iter))
+    # print(next(nums_iter))
+    # print(next(nums_iter))
+    # print(next(nums_iter))
+    # print(next(nums_iter)) # Throws an exception
+
+    # Alternative notation
+
+    print(nums_iter.__next__())
+    print(nums_iter.__next__())
+    print(nums_iter.__next__())
+    print(nums_iter.__next__())
+    print(nums_iter.__next__())
+    print(nums_iter.__next__()) # Throws an exception
